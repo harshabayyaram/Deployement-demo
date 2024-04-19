b953397fcc51101ce8647a42908300293cc28f9e
harsha <bayyaramharsha@gmail.com>
2024-04-19 23:55:44 +0530

    FE error

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Users from './components/Users';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='m-5 p-4 text-center bg-secondary'>React App</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Users.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/users')
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching users:', error);
+            });
+    }, []);
+
+    return (
+        <div className="container mt-5">
+            <h1 className="mb-4 text-center">User Details</h1>
+            <table className="table">
+                <thead className="thead-dark text-center ">
+                    <tr>
+                        <th className='bg-primary'>ID</th>
+                        <th className='bg-primary'>Username</th>
+                        <th className='bg-primary'>Email</th>
+                    </tr>
+                </thead>
+                <tbody className='text-center'>
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Users;
