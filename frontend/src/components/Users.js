f2cced7a89728eeccb42f3e7f0f5f3a9d5e5a3d2
harsha <bayyaramharsha@gmail.com>
2024-04-20 01:37:53 +0530

    Url for get request changed

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -5,7 +5,7 @@ function Users() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:5000/users')
+        axios.get('https://deployement-demo.onrender.com/users')
             .then(response => {
                 setUsers(response.data);
             })
